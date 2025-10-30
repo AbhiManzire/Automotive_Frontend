5702e87e8aa22cca3a15da2a232a5d4273b40765
AbhiManzire <gadhavevaishnavianil283@gmail.com>
2025-10-30 19:14:28 +0530

    airConditioning changes

--- src/component/catalogue/AirConditioning.jsx
+++ src/component/catalogue/AirConditioning.jsx
@@ -150,7 +150,7 @@ const AirConditioning = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-bg-white py-8">
       <div className="max-w-7xl mx-auto px-4">
         <Breadcrumbs />
 
